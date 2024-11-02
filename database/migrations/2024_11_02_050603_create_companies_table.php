7a812d6e7df741cf7aff7335d5e99a3c33f6824c
pansouk <pansouk@gmail.com>
2024-11-02 07:50:01 +0200

    New migration companies and fix user migration

---
+++ database/migrations/2024_11_02_050603_create_companies_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name', 120)->nullable(false);
+            $table->string('address', 255)->nullable(false);
+            $table->string('phone', 20)->nullable(false);
+            $table->string('email', 80)->nullable(false)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
