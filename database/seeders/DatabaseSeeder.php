5d474b1dcc1653720ed2c575483ccd3aff191b80
pansouk <pansouk@gmail.com>
2024-11-04 13:19:56 +0200

    Seed db with companies

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Company;
 use App\Models\Role;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,7 +15,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        // Seed admin role
         Role::factory()->admin()->create();
+
+        // Seed user role
         Role::factory()->user()->create();
+
+        // Seed 50 users
+        User::factory(50)->create();
+
+        // Seed 100 companies and assign users to them
+        Company::factory(100)->create()->each(function(Company $company){
+            $user_role = Role::where('name', 'user')->first();
+            $users = User::where('role_id', $user_role->id)->get();
+            $company->users()->attach($users->random()->id);
+        });
     }
 }
