58f2e1ccdf626d7dd7abb5bd08423a4bcbbd2db4
pansouk <pansouk@gmail.com>
2024-11-04 12:00:15 +0200

    Reformat code

--- database/migrations/2024_11_04_094304_user_company.php
+++ database/migrations/2024_11_04_094304_user_company.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,9 +12,15 @@ return new class extends Migration
     {
         Schema::create('users_companies', function (Blueprint $table) {
             $table->string('user_id')->unsigned()->index();
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
             $table->string('company_id')->unsigned()->index();
-            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+            $table->foreign('company_id')
+                ->references('id')
+                ->on('companies')
+                ->onDelete('cascade');
         });
     }
 
