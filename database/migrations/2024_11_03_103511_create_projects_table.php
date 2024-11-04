3dc267143c248960dcb1173b41ce49d59a2271ef
pansouk <pansouk@gmail.com>
2024-11-04 12:19:07 +0200

    Projects table migration and relation with companies

--- database/migrations/2024_11_03_103511_create_projects_table.php
+++ database/migrations/2024_11_03_103511_create_projects_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Company;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,7 +13,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+            $table->uuid('id')->primary();
+            $table->string('name', '220')->nullable(false);
+            $table->text('description')->nullable();
+            $table->double('budget')->nullable();
+            $table->dateTime('timeline')->nullable();
+            $table->foreignIdFor(Company::class)->constrained()->onDelete('cascade');
+            $table->timestamps();
         });
     }
 
@@ -22,7 +29,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+            Schema::dropIfExists('projects');
         });
     }
 };
